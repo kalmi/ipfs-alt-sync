1578c0a01853ac377f74be7dbfd697fddcdefe73
Kálmán Tarnay <kalmi@users.noreply.github.com>
2016-03-27 22:14:28 +0200

    cleaner stat

--- core/ipfs2dir/stat.go
+++ core/ipfs2dir/stat.go
@@ -7,11 +7,11 @@ type statDataType uint64
 
 const (
 	SeenEntityCount      statDataType = iota
-	SeenFileCount        statDataType = iota
-	ProcessedEntityCount statDataType = iota
-	SkippedEntityCount   statDataType = iota
+	SeenFileCount                     = iota
+	ProcessedEntityCount              = iota
+	SkippedEntityCount                = iota
 	// The next line is needed for the array size in the StatData type
-	numberOfDataPoints statDataType = iota
+	numberOfDataPoints = iota
 )
 
 type StatData struct {
@@ -22,13 +22,17 @@ func (s *StatData) Increment(whatToIncrement statDataType) {
 	atomic.AddUint64(&s.data[whatToIncrement], 1)
 }
 
+func (s *StatData) Read(whatToRead statDataType) uint64 {
+	return atomic.LoadUint64(&s.data[whatToRead])
+}
+
 func (s *StatData) Print() {
 	log.Print("Seen entity count:")
-	log.Print(atomic.LoadUint64(&s.data[SeenEntityCount]))
+	log.Print(s.Read(SeenEntityCount))
 	log.Print("Seen file count:")
-	log.Print(atomic.LoadUint64(&s.data[SeenFileCount]))
+	log.Print(s.Read(SeenFileCount))
 	log.Print("Overwritten file count:")
-	log.Print(atomic.LoadUint64(&s.data[ProcessedEntityCount]))
+	log.Print(s.Read(ProcessedEntityCount))
 	log.Print("Skipped (already up-to-date) file count:")
-	log.Print(atomic.LoadUint64(&s.data[SkippedEntityCount]))
+	log.Print(s.Read(SkippedEntityCount))
 }
